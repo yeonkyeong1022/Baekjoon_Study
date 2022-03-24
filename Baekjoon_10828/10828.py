5be4ffba4ee7b18d4ec78b2d47d577d9ad9b4d12
yeonkyeong1022 <kimi070464@gmail.com>
2022-03-24 21:19:49 +0900

    2022-03-24

---
+++ Baekjoon_10828/10828.py
@@ -0,0 +1,22 @@
+import sys
+N=int(input())
+stack=[]
+for  i in range(N):
+    cmd = sys.stdin.readline().split()
+    if cmd[0] == 'push':
+        stack.append(int(cmd[1]))
+    elif cmd[0] == 'pop':
+        if len(stack)==0 :
+            print("-1")
+        else :
+            print(stack.pop())
+    elif cmd[0] == 'size':
+        print(len(stack))
+    elif cmd[0] == 'empty':
+        print(int(not len(stack)))
+    elif cmd[0] == 'top':
+        if len(stack)==0 :
+            print("-1")
+        else :
+            print(stack[-1])
+    
